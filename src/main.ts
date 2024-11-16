7c7b967f872ca59c3eba811892c4922b3b5c096d
mauricio-hub <mauriciogonzalezdeveloper@gmail.com>
2024-11-16 17:57:33 -0500

    07 solucion

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import './style.css'
-//import './clean-code/04-functions-homework'
-import './clean-code/06-clases-c'
+import './clean-code/07-solucion'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
